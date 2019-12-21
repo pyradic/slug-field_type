9014691fbc1f7cb3cc120577c0319e9699c0868c
Robin Radic <rradic@hotmail.com>
2019-12-21 18:53:28 +0100

    upd

--- src/SlugFieldTypeServiceProvider.php
+++ src/SlugFieldTypeServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Anomaly\SlugFieldType;
 
+use Anomaly\Streams\Platform\Addon\AddonCollection;
 use Anomaly\Streams\Platform\Addon\AddonServiceProvider;
 use Illuminate\Contracts\Support\DeferrableProvider;
 
@@ -13,6 +14,16 @@ use Illuminate\Contracts\Support\DeferrableProvider;
  */
 class SlugFieldTypeServiceProvider extends AddonServiceProvider implements DeferrableProvider
 {
+
+    public function register()
+    {
+        parent::register();
+
+        $this->app->booting(function(){
+            resolve(AddonCollection::class)->put('anomaly.field_type.slug', json_decode(file_get_contents(__DIR__.'/../composer.json'),true));
+        });
+    }
+
 
     /**
      * Return the provided services.
